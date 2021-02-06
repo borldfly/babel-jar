55c983c04f7fbc9dff1411c6fce3b86a91e7e2dc
laoniaofly <ling_huixi@163.com>
2021-02-06 21:38:02 +0800

    尝试修改备注信息

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-import index from './index';
+'use strict';
 
-console.log(index);
+var _console;
+
+var _index = require('./index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var arr = [1, 2, 3, 4];
+(_console = console).log.apply(_console, arr);
